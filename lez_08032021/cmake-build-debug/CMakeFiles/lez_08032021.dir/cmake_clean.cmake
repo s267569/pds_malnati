5cdb500bac37d1e5f3c6a459f275da252ccde7b9
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-03-07 13:00:11 +0100

    Lezione 1 - 08-03-2021

---
+++ lez_08032021/cmake-build-debug/CMakeFiles/lez_08032021.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lez_08032021.dir/main.cpp.o"
+  "lez_08032021"
+  "lez_08032021.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lez_08032021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lez_08032021/cmake-build-debug/CMakeFiles/lez_08032021.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lez_08032021.
+# This may be replaced when dependencies are built.
