ddb0d80454890785571668c0ac5667f2bcfd63e8
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-03-07 14:43:05 +0100

    Prova

---
+++ prova/cmake-build-debug/CMakeFiles/prova.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prova.dir/main.cpp.o"
+  "prova"
+  "prova.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prova.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ prova/cmake-build-debug/CMakeFiles/prova.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for prova.
+# This may be replaced when dependencies are built.
