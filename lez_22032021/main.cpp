e71749098702b278e7a6d9d0a3694117108303fe
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-03-25 16:56:07 +0100

    costruttore di copia

--- lez_22032021/main.cpp
+++ lez_22032021/main.cpp
@@ -5,26 +5,29 @@ public:
     C1(): i(0) {
         std::cout << "costruzione dell'oggetto C1() all'indirizzo" << this
  << std::endl;    } //dobbiamo mettere in modo esplicito i costruttore
+    C1(int v): i(v){
+        std::cout << "costruzione dell'oggetto C1("<<v<<") all'indirizzo" << this<< std::endl;
+    }
+    C1(const C1& other): i(other.i){ //sintassi del costruttore di copia
+        std::cout << "costruzione di copia all'indirizzo " << this
+        << " a partire da un oggetto all'indirizzo " << &other << std::endl;
+    }
     int getI() {return i;}
 };
 
-class C2 {
-    C1 c;
-    float f;
-public:
-    C2(): c(), f(0.0){
-        std::cout << "costruzione dell'ggetto C2() all'indirizzo: " << this << std::endl;
-    } //dobbiamo mettere in modo esplicito i costruttore
-    C1 getC() {return c;}
-    float getF() {return f;}
-};
+void f(C1& c){ //se metto (C1& c) non c'è copia perché passo l'originale e non una copia
+
+}
 
 int main(){
-    C1 c1;
-    C2 c2;
+    C1 c1a{32}, c1b{64}; //due istanze indipendenti
 
-    std::cout << "c1.i = " << c1.getI() << std::endl;
-    std::cout << "c2.c.i = " << c2.getC().getI() << std::endl;
-    std::cout << "c2.c.f = " << c2.getF() << std::endl;
+    //se io volessi che c1b contenesse gli stessi valori di c1a
+    //C1 c1c{c1a}; //sto costruendo il 3° oggetto a partire da quello contenuto in c1a
+    f(c1a);
+
+
+
+
     return 0;
 }
