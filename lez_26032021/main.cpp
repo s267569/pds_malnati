493f4687c31576d33545b5c21c183dbde2d5f489
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-03-28 10:37:43 +0200

    eccezioni

--- lez_26032021/main.cpp
+++ lez_26032021/main.cpp
@@ -10,13 +10,30 @@ public:
     }
     C& operator=(const C& other){ //operatore di assegnazione
         std::cout << "C @" << this << " = C @" << (void *)&other << std::endl;
+        throw std::exception(); //simuliamo che qui facciamo una operazione che fallisce
         return *this;
     }
 };
 
-int main() {
+void f(){
     C c1;
     C c2;
+
     c2=c1;
+    std::cout << "Di qui non passa!"<<std::endl;
+}
+
+void g(){
+    std::cout << "Entro in g()"<<std::endl;
+    f();
+    std::cout << "Esco da g()"<<std::endl;
+}
+
+int main() {
+    try{
+        g();
+    }catch (std::exception& e){
+        std::cout << "Eccezione!"<<std::endl;
+    }
     return 0;
 }
