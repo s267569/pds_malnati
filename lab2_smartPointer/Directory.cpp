d45484f829dd60c323b03d112c7dc5e754e634e1
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-05-17 20:19:21 +0200

    lab2_smartPointer

--- lab2_smartPointer/Directory.cpp
+++ lab2_smartPointer/Directory.cpp
@@ -6,84 +6,17 @@
 #include <iostream>
 #include <iomanip>
 
-Directory* Directory::root;
+//Questo perché Base è la classe da cui eredito, quindi si usa mettere Base.
+//Usiamo il costruttore di Base perché il nome è una var privata di Base
+Directory::Directory(std::string name) : Base(name){}
 
-Directory* Directory::getRoot() {
-    if (root == nullptr)
-        root = new Directory("/", nullptr);
-    return root;
-}
-
-Directory::Directory(std::string name, Directory* father):Base(name),father(father),self(this) {
-    std::cout << "constructor @" << this << std::endl;
-}
-
-Directory* Directory::addDirectory(const std::string& name) {
-    return children.count(name) == 0 ?
-           children.emplace(std::make_pair(name, new Directory(name, this))).first->second :
-           children.find(name)->second;
-}
-
-Directory* Directory::get(const std::string& name) {
-    return name == "." ? this->self :
-           (name == ".." && getName() != "/") ? this->father :
-           children.count(name) == 1 ? children.find(name)->second : nullptr;
-}
-
-bool Directory::remove(const std::string& name) {
-    if (children.count(name) == 1) {
-        auto it = children.find(name);
-        delete it->second;
-        children.erase(it);
-        return true;
+std::shared_ptr<Directory> Directory::getRoot(){
+    if (root == nullptr){
+        root = new Directory("/");
+        std::shared_ptr<Directory> _shared_root_ = std::shared_ptr<Directory>(root);
+        root->parent = std::shared_ptr<Directory>(nullptr); //oppure root->parent=nullptr;
+        root->self = _shared_root_;
+        return _shared_root_;
     }
-    return false;
-}
-
-bool Directory::move(const std::string& name, Directory* target) {
-    if (children.count(name) == 1) {
-        Directory* tmp = children.find(name)->second;
-        if (target->children.count(name) == 0) {
-            tmp->father = target;
-            target->children.insert(std::make_pair(name, tmp));
-            //children.find(name)->second = nullptr;
-            children.erase(name); // remove entry containing pointer to target
-            return true;
-        }
-
-    }
-    return false;
-}
-
-bool Directory::copy(const std::string& name, Directory* target) {
-    if (children.count(name) == 1) {
-        Directory* source = children.find(name)->second;
-        if (target->children.count(name) == 0) {
-            Directory* tmp = new Directory(name, target);
-            for (auto it = source->children.cbegin(); it != source->children.cend(); ++it)
-                tmp->children.insert(std::make_pair(it->first, it->second));
-            target->children.insert(std::make_pair(name, tmp));
-            return true;
-        }
-    }
-    return false;
-}
-
-int Directory::mType() const {
-    return 0;
-}
-
-
-void Directory::ls(int indent) const {
-    std::cout << (getName()!="/"?std::string(indent,' '):"") <<"[+] " <<  getName() << std::endl;
-    for (auto it = children.cbegin(); it != children.cend(); ++it)
-        it->second->ls(indent*2);
-
-}
-
-Directory::~Directory() {
-    std::cout << "destructor @" << this << std::endl;
-    for (auto it = children.begin(); it != children.end(); ++it) {
-        delete it->second;
-    }
+    return root->self.lock();
 }
--- lab2_smartPointer/Directory.h
+++ lab2_smartPointer/Directory.h
@@ -6,23 +6,24 @@
 #define LAB2_SMARTPOINTER_DIRECTORY_H
 #include "Base.h"
 #include "File.h"
+#include <memory>
 #include <map>
 
 class Directory : public Base {
     static Directory* root;
     std::map<std::string, Base*> children;
-    Directory* parent;
-    Directory* self;
+    std::weak_ptr<Directory> parent;
+    std::weak_ptr<Directory> self;
     Directory() = delete;
 
 public:
     //explicit è semplicemente per marcare un costruttore che ha un solo parametro per rendere chiara la sintassi
     explicit Directory(std::string name);
     ~Directory() noexcept;
-    static Directory* getRoot();
-    static bool removeRoot();
-    Directory* addDirectory(const std::string& name);
-    File* addFile(const std::string & name, uintmax_t size, uintmax_t data);
+    static std::shared_ptr<Directory> getRoot();
+    //static bool removeRoot();
+    std::shared_ptr<Directory> addDirectory(const std::string& name);
+    std::shared_ptr<File> addFile(const std::string & name, uintmax_t size, uintmax_t data);
     Base* get(const std::string & name);
     Directory* getDirectory(const std::string & name);
     File* getFile(const std::string & name);
