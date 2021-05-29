c8a2757e51939a7a9f111a940629566f53f73ef3
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-05-29 12:44:10 +0200

    thread detach

--- thread2/main.cpp
+++ thread2/main.cpp
@@ -3,14 +3,30 @@
 #include <mutex>
 
 int n = 0;
+std::mutex m;
+void f1(){
+    for(int i=0; i< 10000000; i++){
+        std::lock_guard<std::mutex> lg(m);
+        n++;
+    }
+}
 
+void f2(){
+    for(int i=0; i< 10000000; i++){
+        std::lock_guard<std::mutex> lg(m);
+        n++;
+    }
+}
 
-
+std::thread createThead() {
+    std::thread t(f1);
+    return std::move(t);
+}
 int main() {
-    std::thread t1(f1);
-    std::thread t2(f2);
+    std::thread t1 = createThead();
+    std::thread t2 = createThead();
 
-    t1.join();
+    t1.detach();
     t2.detach();
 
     std::cout << "Alla fine n vale : " << n << std::endl;
