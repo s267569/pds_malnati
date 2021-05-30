2c3c55b8f5ac1ed5ce2c04aa9b958c85642c657e
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-05-30 17:04:38 +0200

    lab 5 es 1

---
+++ lab5_1/cmake-build-debug/CMakeFiles/lab5_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab5_1.dir/main.cpp.o"
+  "lab5_1"
+  "lab5_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab5_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
