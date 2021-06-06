5c24ede6351d3e9715c0b613ea8c8cd9dce15226
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-06-06 17:42:29 +0200

    Laboratorio 5 esercizio 3

--- lab5_3/main.cpp
+++ lab5_3/main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <mutex>
+#include <thread>
+#include <condition_variable>
 
+class CountingSemaphore{
+    int count;
+    std::mutex m;
+    std::condition_variable cv;
+public:
+    CountingSemaphore(int n){
+        count = n;
+    }
+    CountingSemaphore(){
+        count = 0;
+    }
+    void acquire(){
+        std::unique_lock<std::mutex> lk(m);
+        if(count <=0){
+            cv.wait(lk);
+        }else{
+            count --;
+            return;
+        }
+    }
+    void release(){
+        std::lock_guard<std::mutex> lg(m);
+        count++;
+        cv.notify_one();
+    }
+};
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
