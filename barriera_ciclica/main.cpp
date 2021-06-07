33ca4db482b03c349e2502062458c84f313bdd5c
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-06-07 19:03:01 +0200

    esercizio barriera ciclica

--- barriera_ciclica/main.cpp
+++ barriera_ciclica/main.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <vector>
 
 int n; //numero thread bloccati
 class BarrieraCiclica{
@@ -14,21 +15,36 @@ public:
     }
     void attendi(){
         //blocca il thread chiamante fino a che altri N_1 thread non risultano bloccato insieme ad esso
-        if(n == countT){
+        if(n == countT-1){
             std::lock_guard<std::mutex> lk(m);
             n=0; //ora non sarà nessuno bloccato
             cv.notify_all();
             return;
         }
-        if(n<=countT-1){
+        else{
             std::unique_lock<std::mutex> ul(m);
             n++;
-            cv.wait(ul); //manca controllo condizione spuria (forse non serve)
+            while(n!=countT-1)
+                cv.wait(ul);
         }
     }
 };
 int main() {
     BarrieraCiclica bc(3);
+    std::vector<std::thread> v;
+    for(int i=0; i<5; i++){
+        std::thread t([i, &bc](){
+            std::cout << "Thread " << i << " started" << std::endl;
+            bc.attendi();
+            std::cout << "Thread " << i << " finished" << std::endl;
+        });
+        v.emplace_back(std::move(t));
+    }
+
+    for (int i=0; i<v.size(); i++){
+        v[i].join();
+    }
+
 
     return 0;
 }
