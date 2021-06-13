a65c4cc6dc64075eda415f2d123de0a659ae0e26
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-06-13 21:32:03 +0200

    Esempio con async() e future

--- cap14_slide7/main.cpp
+++ cap14_slide7/main.cpp
@@ -2,8 +2,8 @@
 #include <future>
 #include <string>
 
-double f1(double p2){
-    double f1 = p2+1;
+double f1(double p2, double p3){
+    double f1 = p2+1+p3;
     return f1;
 }
 
@@ -14,8 +14,9 @@ int f2(int p2){
 
 int main() {
     //calcola f1 + f2
-    std::future<double> future1 = std::async(f1,3.14);
+    std::future<double> future1 = std::async(f1,3.14, 1);
     double res2 = f2(18);
+    future1.wait();
     double res1 = future1.get();
     double result = res1+res2;
 
