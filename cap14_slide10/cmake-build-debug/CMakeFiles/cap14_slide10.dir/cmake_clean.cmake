36e3df7c06d1ed26493d5ca3820a4ecfb6b08e66
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-06-13 21:36:42 +0200

    Esempio future async con std::launch::async

---
+++ cap14_slide10/cmake-build-debug/CMakeFiles/cap14_slide10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cap14_slide10.dir/main.cpp.o"
+  "cap14_slide10"
+  "cap14_slide10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cap14_slide10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cap14_slide10/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <future>
+#include <vector>
+#include <numeric>
+
+template <typename Iter>
+int parallel_sum(Iter begin, Iter end){
+    typename Iter::difference_type len = end-begin;
+    if(len<1000)
+        return std::accumulate(begin, end, 0);
+    Iter mid = begin+len/2;
+    auto handle = std::async(std::launch::async, parallel_sum<Iter>, mid, end);
+    int sum= parallel_sum(begin, mid);
+    return sum+handle.get();
+}
+
+int main() {
+    std::vector<int> v(1000, 1);
+    std::cout << "Somma: " << parallel_sum(v.begin(), v.end()) << '\n';
+    return 0;
+}
