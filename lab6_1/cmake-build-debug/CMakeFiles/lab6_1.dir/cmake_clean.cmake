2e7a4a3a4a46f8abb63801aae8cc87976305c85d
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-06-13 15:46:27 +0200

    Laboratorio 6 esercizio 1

---
+++ lab6_1/cmake-build-debug/CMakeFiles/lab6_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab6_1.dir/main.cpp.o"
+  "lab6_1"
+  "lab6_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab6_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
