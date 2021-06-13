e465adc10b3dd8f3aabf136d2ac1c6c6d65dbcb7
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-06-13 23:21:33 +0200

    Laboratorio 6 esercizio 2

--- lab6_es2/main.cpp
+++ lab6_es2/main.cpp
@@ -2,28 +2,21 @@
 #include <future>
 #include <vector>
 
-std::vector<int> parallel_sort(std::vector<int> v, int start, int end){
-    int i;
+template <typename Iter>
+std::vector<int> parallel_sort(std::vector<int> v, Iter start, Iter end){
+    Iter mid = v.size()/2;
     int size_vector = v.size();
     std::vector<int> first_vector;
     std::vector<int> second_vector;
-    for(i=0; i< end; i++){
-        first_vector[i] = v[i];
-    }
+
     std::sort(first_vector.begin(), first_vector.end());
 
-    for (i=start; i<v.size(); i++){
-        second_vector[i] = v[i];
-    }
-    std::sort(second_vector.begin(), second_vector.end());
-
-    auto handle = std::async(std::launch::async, parallel_sort, v, 0, size_vector/2);
-    std::vector<int> sum = parallel_sort(v,size_vector/2+1,size_vector);
+    auto handle = std::async(std::launch::async, parallel_sort, v, mid, end);
+    std::vector<int> sum = parallel_sort(v,start,mid);
     return handle.get();
 }
 int main() {
     std::vector<int> vettore = {2,1,3,4,5,6};
-    std::vector<int> res=parallel_sort(vettore, 0, vettore.size());
-
+    std::cout << parallel_sort(vettore, vettore.begin(), vettore.end()) << std::endl;
     return 0;
 }
