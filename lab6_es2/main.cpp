67716f439aa575dcc2c7bfeaa4374340a0598567
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-06-14 22:05:24 +0200

    Laboratorio 6 esercizio 2

--- lab6_es2/main.cpp
+++ lab6_es2/main.cpp
@@ -11,15 +11,16 @@ std::vector<int> parallel_sort(std::vector<int> v, int start, int end, int level
     }
     int mid = (end-start)/2 + start;
     auto handle = std::async(std::launch::async, parallel_sort, v, mid, end, level-1);
-    std::vector<int> result = parallel_sort(v,start,mid, level-1);
+    auto handle_result = std::async(std::launch::async, parallel_sort, v, start, mid, level-1);
     std::vector<int> result_handle = handle.get();
+    std::vector<int> result = handle_result.get();
     std::vector<int> final;
     std::merge(result_handle.begin(), result_handle.end(), result.begin(), result.end(), std::back_inserter(final));
     return final;
 }
 int main() {
-    std::vector<int> vettore = {2,1,3,4,5,6};
-    std::vector<int> result = parallel_sort(vettore, 0, vettore.size(), 2);
+    std::vector<int> vettore = {2,1,3,4,5,69,1,7};
+    std::vector<int> result = parallel_sort(vettore, 0, vettore.size(), 3);
 
     for(int i:result)
         std::cout << i << std::endl;
