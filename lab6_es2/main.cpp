eba1c0548a89e96715f8dc54f81cbd4037ab1ce0
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-06-14 21:59:22 +0200

    Laboratorio 6 esercizio 2

--- lab6_es2/main.cpp
+++ lab6_es2/main.cpp
@@ -2,21 +2,26 @@
 #include <future>
 #include <vector>
 
-template <typename Iter>
-std::vector<int> parallel_sort(std::vector<int> v, Iter start, Iter end){
-    Iter mid = v.size()/2;
-    int size_vector = v.size();
-    std::vector<int> first_vector;
-    std::vector<int> second_vector;
-
-    std::sort(first_vector.begin(), first_vector.end());
-
-    auto handle = std::async(std::launch::async, parallel_sort, v, mid, end);
-    std::vector<int> sum = parallel_sort(v,start,mid);
-    return handle.get();
+std::vector<int> parallel_sort(std::vector<int> v, int start, int end, int level){
+    //condizione di terminazione
+    if(level==0){
+        std::sort(v.begin()+start, v.begin()+end);
+        std::vector<int> final(v.begin()+start, v.begin()+end);
+        return final;
+    }
+    int mid = (end-start)/2 + start;
+    auto handle = std::async(std::launch::async, parallel_sort, v, mid, end, level-1);
+    std::vector<int> result = parallel_sort(v,start,mid, level-1);
+    std::vector<int> result_handle = handle.get();
+    std::vector<int> final;
+    std::merge(result_handle.begin(), result_handle.end(), result.begin(), result.end(), std::back_inserter(final));
+    return final;
 }
 int main() {
     std::vector<int> vettore = {2,1,3,4,5,6};
-    std::cout << parallel_sort(vettore, vettore.begin(), vettore.end()) << std::endl;
+    std::vector<int> result = parallel_sort(vettore, 0, vettore.size(), 2);
+
+    for(int i:result)
+        std::cout << i << std::endl;
     return 0;
 }
