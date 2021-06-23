7890e3b33ec468eb016eb44f04ff4e597c812bcb
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-06-23 11:45:02 +0200

    Laboratorio 6 esercizio 3 - Implementazione dei thread pool

--- lab6_3/ThreadPool.cpp
+++ lab6_3/ThreadPool.cpp
@@ -8,8 +8,30 @@ ThreadPool::ThreadPool(int min_threads, int max_threads, int max_size) : min_thr
                                                                         max_threads_(max_threads),
                                                                         max_size_(max_size)     {
     active_threads=0;
-
+    auto execution = [this](){
+            while(true) {
+                //operare sulla coda è la sezione critica, l'operazione sul packaged_trask invece è una operazione che
+                //posso eseguire contemporaneamente agli altri
+                std::unique_lock ul{m_tasks};
+                cv_task_ready.wait(ul,
+                                   [&]() { return !tasks.empty(); }); //catturo per reference: catturo tutto ciò di prima per reference.
+                //potevo anche catturare anche solo &tasks.
+                //La pop in C++ va sempre fatta in due passaggi
+                std::packaged_task<void()> task_to_execute = std::move(
+                        tasks.front()); //packaged_task non è copiabile ma solo movibile
+                tasks.pop_front();
+                ul.unlock(); //Non abbiamo più bisogno di operare in maniera protetta
+                task_to_execute();
+            }
+    };
+    for (int i=0; i<min_threads; i++){
+        executors.emplace_back(std::thread(execution)); //con emplace_back si poteva anche solo scrivere executors.emplace_back(execution)
+    }
 }
-void execute(std::packaged_task<void()> task);
+void ThreadPool::execute(std::packaged_task<void()> task){
+    std::unique_lock ul(m_tasks);
+    tasks.push_back(std::move(task));
+    ul.unlock(); //anche se non serve perché subito dopo vi è la chiusura graffe
+}
 void finish();
 ThreadPool::~ThreadPool();
