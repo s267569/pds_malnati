2684930189b6c53273a802eaa305d235081990cd
Domenico Cefalo <cefalo.domenico@gmail.com>
2021-06-24 10:47:16 +0200

    Packaged_task esempio

---
+++ packaged_task/cmake-build-debug/CMakeFiles/packaged_task.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/packaged_task.dir/main.cpp.o"
+  "packaged_task"
+  "packaged_task.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/packaged_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
